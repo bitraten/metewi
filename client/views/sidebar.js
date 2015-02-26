77b98b925f6417287f03ea4b58b191b43f0d9f5b
bitraten <rph@bitraten.de>
2015-02-26 23:29:02 +0100

    Add itemInsert Meteor.method

--- client/views/sidebar.js
+++ client/views/sidebar.js
@@ -1,15 +1,22 @@
+"use strict";
+
 Template.sidebar.events({
     'keypress input.add-item': function(e) {
         if (e.which === 13) {
             var title = $(e.target).val();
-            var item = {'title': title, 'created_at': Date.now()};
 
-            Items.insert(item, function(error, _id) {
-                if(error) {
-                    alert(error.reason);
-                } else {
-                    Router.go('item.edit', {'_id': _id});
-                }
+            Meteor.call('itemInsert', title, function(error, result){
+              // display the error to the user and abort
+              if (error) {
+                return alert(error.reason);
+              }
+
+              // show this result but route anyway
+              if (result.itemExists){
+                alert('An item with this title already exists!');
+              }
+
+                Router.go('item.edit', {_id: result._id});
             });
         }
     }
